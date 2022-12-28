9492f910636bb322a95271c08fc255c67d759561
Andrew Martin <114.amart@gmail.com>
2022-12-28 15:10:51 +0000

    added intial structures for diplomacy game

---
+++ diplomacy/Game.py
@@ -0,0 +1,12 @@
+class Game:
+    '''Class to instantiate a game of diplomacy.
+    
+    It will handle the initialisation or loading of the game:
+        + Load in the map that the game will be played on.
+        + Load in profiles for each of the players or teams
+        
+    It will also handle the logic for executing turns and saving game states.
+    '''
+
+    def __init__(self):
+        pass
